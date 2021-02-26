823f8b27d5ed40e5264865fff9a157fbe2dd83bf
James Wyn <thisisjameswyn@gmail.com>
2021-02-26 11:10:11 -0800

    add more tests and object logic

--- PierresBakery.Tests/ModelTests/PierresBakeryTest.cs
+++ PierresBakery.Tests/ModelTests/PierresBakeryTest.cs
@@ -7,17 +7,27 @@ namespace PierresBakery.Tests
   public class BakeryTests
   {
     [TestMethod]
+    public void OrderConstructor_CreateInstanceOfOrder_OrderCalc()
+    {
+      OrderCalc testOrder = new OrderCalc(0,0);
+      Assert.AreEqual(typeof(OrderCalc), testOrder.GetType());
+    }
+
+    [TestMethod]
+    public void OrderConstructor_ReturnAmount_OrderCalc()
+    {
+      int _pAmount = 5;
+      int _bAmount = 5;
+      OrderCalc testOrder = new OrderCalc(_pAmount,_bAmount);
+      Assert.AreEqual(_bAmount, testOrder.BreadAmount);
+      Assert.AreEqual(_pAmount, testOrder.PastryAmount);
+    }
+
+    [TestMethod]
     public void PastryConstructor_CreateInstanceOfPastry_Pastry()
     {
       Pastry testPastry = new Pastry(0);
       Assert.AreEqual(typeof(Pastry), testPastry.GetType());
-    }
-
-    [TestMethod]
-    public void BreadConstructor_CreateInstanceOfBread_Bread()
-    {
-      Bread testBread = new Bread(0);
-      Assert.AreEqual(typeof(Bread), testBread.GetType());
     }
 
     [TestMethod]
@@ -27,6 +37,14 @@ namespace PierresBakery.Tests
       Pastry testPastry = new Pastry(amountToBuy);
       Assert.AreEqual(testPastry.PurchaseAmount, amountToBuy);
     }
+
+    [TestMethod]
+    public void BreadConstructor_CreateInstanceOfBread_Bread()
+    {
+      Bread testBread = new Bread(0);
+      Assert.AreEqual(typeof(Bread), testBread.GetType());
+    }
+
 
     [TestMethod]
     public void BreadConstructor_ReturnAmount_Int()
