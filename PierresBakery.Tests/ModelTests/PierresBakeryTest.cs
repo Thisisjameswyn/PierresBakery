28843fb28d18ed16ef2d096f3321dc03cf3410d1
James Wyn <thisisjameswyn@gmail.com>
2021-03-02 10:54:59 -0800

    remove excess commented out lines

--- PierresBakery.Tests/ModelTests/PierresBakeryTest.cs
+++ PierresBakery.Tests/ModelTests/PierresBakeryTest.cs
@@ -4,54 +4,101 @@ using PierresBakery.Models;
 namespace PierresBakery.Tests
 {
   [TestClass]
-  public class BakeryTests
+  public class OrderHandlerTests
   {
     [TestMethod]
     public void OrderConstructor_CreateInstanceOfOrder_OrderCalc()
     {
-      OrderCalc testOrder = new OrderCalc(0,0);
-      Assert.AreEqual(typeof(OrderCalc), testOrder.GetType());
+      OrderHandler testOrder = new OrderHandler();
+      Assert.AreEqual(typeof(OrderHandler), testOrder.GetType());
     }
 
     [TestMethod]
-    public void OrderConstructor_ReturnAmount_OrderCalc()
+    public void GetOrderAmount_ReturnCorrectAmount_Int()
     {
-      int _pAmount = 5;
-      int _bAmount = 5;
-      OrderCalc testOrder = new OrderCalc(_pAmount,_bAmount);
-      Assert.AreEqual(_bAmount, testOrder.BreadAmount);
-      Assert.AreEqual(_pAmount, testOrder.PastryAmount);
+      int _pAmount = 0;
+      int _bAmount = 0;
+      OrderHandler testOrder = new OrderHandler();
+      Assert.AreEqual(_bAmount, testOrder.GetOrderAmount("Bread"));
+      Assert.AreEqual(_pAmount, testOrder.GetOrderAmount("Pastry"));
     }
 
     [TestMethod]
-    public void PastryConstructor_CreateInstanceOfPastry_Pastry()
+    public void SetPurchaseAmount_SetCorrectAmount_Int()
     {
-      Pastry testPastry = new Pastry(0);
-      Assert.AreEqual(typeof(Pastry), testPastry.GetType());
+      int _pAmount = 8;
+      int _bAmount = 3;
+      OrderHandler testOrder = new OrderHandler();
+      testOrder.SetPurchaseAmount(_bAmount, "Bread");
+      testOrder.SetPurchaseAmount(_pAmount, "Pastry");
+      Assert.AreEqual(_bAmount, testOrder.GetOrderAmount("Bread"));
+      Assert.AreEqual(_pAmount, testOrder.GetOrderAmount("Pastry"));
     }
 
     [TestMethod]
-    public void PastryConstructor_ReturnAmount_Int()
+    public void PushOrder_SetCorrectCost_Int()
     {
-      int amountToBuy = 5;
-      Pastry testPastry = new Pastry(amountToBuy);
-      Assert.AreEqual(testPastry.PurchaseAmount, amountToBuy);
+      int _pAmount = 8;
+      int _bAmount = 3;
+      int figuredBreadCost = 10;
+      int figuredPastryCost = 14; 
+      OrderHandler testOrder = new OrderHandler();
+      testOrder.SetPurchaseAmount(_bAmount, "Bread");
+      testOrder.SetPurchaseAmount(_pAmount, "Pastry");
+      testOrder.PushOrder("Bread");
+      testOrder.PushOrder("Pastry");
+      Assert.AreEqual(figuredBreadCost, testOrder.GetOrderCost("Bread"));
+      Assert.AreEqual(figuredPastryCost, testOrder.GetOrderCost("Pastry"));
     }
 
     [TestMethod]
-    public void BreadConstructor_CreateInstanceOfBread_Bread()
+    public void GetOrderCost_ReturnCorrectCost_Int()
     {
-      Bread testBread = new Bread(0);
-      Assert.AreEqual(typeof(Bread), testBread.GetType());
+      int _pAmount = 8;
+      int _bAmount = 3;
+      int figuredBreadCost = 10;
+      int figuredPastryCost = 14; 
+      OrderHandler testOrder = new OrderHandler();
+      testOrder.SetPurchaseAmount(_bAmount, "Bread");
+      testOrder.SetPurchaseAmount(_pAmount, "Pastry");
+      testOrder.PushOrder("Bread");
+      testOrder.PushOrder("Pastry");
+      Assert.AreEqual(figuredBreadCost, testOrder.GetOrderCost("Bread"));
+      Assert.AreEqual(figuredPastryCost, testOrder.GetOrderCost("Pastry"));
     }
 
+    [TestMethod]
+    public void EmptyCart_ReturnCorrectAmount_Int()
+    {
+      int _pAmount = 8;
+      int _bAmount = 3;
+      int expectValue = 0;
+      OrderHandler testOrder = new OrderHandler();
+      testOrder.SetPurchaseAmount(_bAmount, "Bread");
+      testOrder.SetPurchaseAmount(_pAmount, "Pastry");
+      testOrder.PushOrder("Bread");
+      testOrder.PushOrder("Pastry");
+      testOrder.EmptyCart();
+      Assert.AreEqual(expectValue, testOrder.GetOrderCost("Bread"));
+      Assert.AreEqual(expectValue, testOrder.GetOrderCost("Pastry"));
+      Assert.AreEqual(expectValue, testOrder.GetOrderAmount("Bread"));
+      Assert.AreEqual(expectValue, testOrder.GetOrderAmount("Pastry"));
+    }
 
     [TestMethod]
-    public void BreadConstructor_ReturnAmount_Int()
+    public void CostPush_ReturnCorrectDealAmount_Int()
     {
-      int amountToBuy = 5;
-      Bread testBread = new Bread(amountToBuy);
-      Assert.AreEqual(testBread.PurchaseAmount, amountToBuy);
+      int _bAmount = 3;
+      int _pAmount = 8;
+      int bExpectValue = 10;
+      int pExpectValue = 14;
+      OrderHandler testOrder = new OrderHandler();
+      testOrder.SetPurchaseAmount(_bAmount, "Bread");
+      testOrder.SetPurchaseAmount(_pAmount, "Pastry");
+      testOrder.PushOrder("Bread");
+      testOrder.PushOrder("Pastry");
+      Assert.AreEqual(bExpectValue, testOrder.GetOrderCost("Bread"));
+      Assert.AreEqual(pExpectValue, testOrder.GetOrderCost("Pastry"));
     }
   }
 }
